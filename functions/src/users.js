12aa6ccc88f001e2796c62917b40bcac3252ddd4
David Radjai <dradjai@yahoo.com>
2023-04-27 23:06:27 -0400

    added validation rules to signup api

--- functions/src/users.js
+++ functions/src/users.js
@@ -1,10 +1,20 @@
+import { FieldValue } from 'firebase-admin/firestore';
 import { db } from "./dbConnect.js";
 
 const coll = db.collection('users');
 
 export async function addNewUser(req, res) {
-  let newUser = req.body;
-  newUser.email = newUser.email.toLowerCase();
+  const { email, password } = req.body;
+  if(!email || password.length < 4) {
+    res.status(400).send({ message: "Email and password required and the length must be greater than 3."})
+    return 
+  }
+
+  const newUser = {
+    email: email.toLowerCase(),
+    password,
+    createdAt: FieldValue.serverTimestamp(),
+  }
   await coll.add(newUser);
   res.status(201).send({message: "Registered"});
 
